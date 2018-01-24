48f01ad2760e5f266875ba9d648366fe467e7e91
Fernando Ayala <fja250@email.vccs.edu>
2018-01-24 09:52:30 -0500

    added alert and input dialog

--- Rectangle.java
+++ Rectangle.java
@@ -8,17 +8,22 @@ import javafx.scene.control.Alert;
 public class Rectangle extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		TextInputDialog textInputDialog = new TextInputDialog("Enter height and width");
-		Optional<String> result = textInputDialog.showAndWait();
-		String widthInput = result.get();
-		double width = Double.parseDouble(widthInput);
-
 		
-		String heightInput = result.get();
-		double height = Double.parseDouble(heightInput);		
+		TextInputDialog input = new TextInputDialog();
+		input.setTitle("Rectangle Calc");
+		input.setHeaderText("Perimeter of Rectangle");
+		input.setContentText("Enter height & width");
+		input.showAndWait();
+
+		double height = Double.parseDouble(input.getResult());
+		double width = Double.parseDouble(input.getResult());	
 		
 		Alert alert = new Alert(AlertType.INFORMATION);
-		Alert.setContentText("Your perimeter is" + 2*height + 2*width);
+		alert.setTitle("Rectangle");
+		alert.setHeaderText("Perimeter");
+		alert.setContentText("Your perimeter is" + 2*height + 2*width);
+		alert.showAndWait();
+
 		}
 	public static void main(String [] args) {
 		launch(args);	
