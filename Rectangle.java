64de2dcf3d7dbbd00dc6ec09fa942c48dd8559a8
Fernando Ayala <fja250@email.vccs.edu>
2018-01-23 21:05:26 -0500

    add import statements and fix erros

--- Rectangle.java
+++ Rectangle.java
@@ -1,10 +1,15 @@
-public class Rectangle {
-	@override
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.TextInputDialog;
+import java.util.Optional;
+
+public class Rectangle extends Application {
+	@Override
 	public void start(Stage primaryStage) {
 		TextInputDialog textInputDialog = new TextInputDialog("Geometric Calculator");
-		Optional<String> result = texstInputDialog.showAndWait();
+		Optional<String> result = textInputDialog.showAndWait();
 		String widthInput = result.get();
-		double width = Double.parseDouble(input);
+		double width = Double.parseDouble(widthInput);
 	}
 	public static void main(String [] args) {
 		launch(args);	
