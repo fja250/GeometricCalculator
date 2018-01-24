e0042879c8176ebd4020ad15dad151879eaf1a58
Fernando Ayala <fja250@email.vccs.edu>
2018-01-24 09:07:39 -0500

    added result alert and formula to calculate perimeter

--- Rectangle.java
+++ Rectangle.java
@@ -2,17 +2,23 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.control.TextInputDialog;
 import java.util.Optional;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
 
 public class Rectangle extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		TextInputDialog textInputDialog = new TextInputDialog("Geometric Calculator");
+		TextInputDialog textInputDialog = new TextInputDialog("Enter height and width");
 		Optional<String> result = textInputDialog.showAndWait();
 		String widthInput = result.get();
 		double width = Double.parseDouble(widthInput);
 
+		
 		String heightInput = result.get();
 		double height = Double.parseDouble(heightInput);		
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		Alert.setContentText("Your perimeter is" + 2height + 2width);
 
 		}
 	public static void main(String [] args) {
